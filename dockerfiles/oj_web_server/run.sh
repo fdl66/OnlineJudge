9e07a98ba1c3814eeee7deeeabbe0cceb4da841e
fdl66 <fan0816fan@163.com>
2017-11-04 15:55:04 +0800

    更新dockerfiles,celery部署时存在问题，由于user数据库的原因启动不起来

--- dockerfiles/oj_web_server/run.sh
+++ dockerfiles/oj_web_server/run.sh
@@ -10,17 +10,18 @@ if [ ! -f "$ca_base_dir/server.key" ]; then
 fi
 
 find /code -name "*.pyc" -delete
-# python -m compileall /code
+python -m compileall /code
 chown -R nobody:nogroup /code/log /code/test_case /code/upload /code/ssl
 cd /code
 n=0
 until [ $n -ge 5 ]
 do
-    python tools/create_db.py &&
-    python manage.py migrate --no-input && 
-    python manage.py migrate --database=submission --no-input &&
-    python manage.py initadmin && break
+    python tools/create_db.py && sleep 1
+    python manage.py migrate  && sleep 1
+    python manage.py migrate --database=submission && sleep 1
+    python manage.py initadmin  && break
     n=$(($n+1))
     sleep 8
 done
+sleep 1
 exec supervisord -c /code/dockerfiles/oj_web_server/supervisord.conf
